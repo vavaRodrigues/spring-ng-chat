666970324c4ac8a8a9e8c6ce886cfa8a4b534882
Wagner Rodrigues <wra.silva@gmail.com>
2015-09-01 21:59:06 -0300

    first commit

---
+++ src/main/java/br/com/chat/config/AppConfig.java
@@ -0,0 +1,13 @@
+package br.com.chat.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.stereotype.Controller;
+
+@Configuration
+@ComponentScan(basePackages = "br.com.chat", excludeFilters = {
+    @ComponentScan.Filter(value = Controller.class, type = FilterType.ANNOTATION),
+    @ComponentScan.Filter(value = Configuration.class, type = FilterType.ANNOTATION)
+})
+public class AppConfig {
+
+}
---
+++ src/main/java/br/com/chat/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package br.com.chat.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@ComponentScan(basePackages = "br.com.chat.controller")
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry config) {
+    config.enableSimpleBroker("/topic");
+    config.setApplicationDestinationPrefixes("/app");
+  }
+
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry.addEndpoint("/chat").withSockJS();
+  }
+}
